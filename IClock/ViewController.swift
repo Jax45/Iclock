dccb4967c43474ed01c2ed0fa87f4af603567427
Jackson Hoenig <jack@hoenig.net>
2019-11-16 00:37:15 -0600

    added digital need the two new screens now then persist

--- IClock/ViewController.swift
+++ IClock/ViewController.swift
@@ -11,7 +11,12 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var mainView: MainView!
-    @IBOutlet weak var hourTen: UILabel!
+    @IBOutlet weak var HourLabel: UILabel!
+    @IBOutlet weak var MinuteLabel: UILabel!
+    @IBOutlet weak var SecondLabel: UILabel!
+    @IBOutlet weak var MilliLabel: UILabel!
+    @IBOutlet weak var ampmLabel: UILabel!
+    
     
     var clockView = ClockView(frame: .zero)
     override func viewDidLoad() {
@@ -24,14 +29,33 @@ class ViewController: UIViewController {
     @objc func updateClock(){
         let clock = mainView.subviews[0] as? ClockView
         let date = Date()
-        let hour = Calendar.current.component(.hour, from:date)
+        var hour = Calendar.current.component(.hour, from:date)
         let min = Calendar.current.component(.minute, from:date)
         let sec = Calendar.current.component(.second, from:date)
+        let ns = Calendar.current.component(.nanosecond, from: date)
+        let ms = ns / 1000000
+        
+        
+        MinuteLabel.text = String(min)
+        SecondLabel.text = String(sec)
+        MilliLabel.text = String(ms)
+        if hour - 12 >= 0{
+            hour -= 12
+            if hour == 0{ hour = 12}
 
+            HourLabel.text = String(hour)
+            ampmLabel.text = "PM"
+        }
+        else{
+            if hour == 0{ hour = 12}
+            HourLabel.text = String(hour)
+            ampmLabel.text = "AM"
+        }
         clock?.timePassed(hour: hour, minute: min, second: sec)
     }
     
 
-
+    
+    
 }
 
